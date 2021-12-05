50c9a9684cbe994337d0bd6430c25dfa936609b8
Furkan Detseli <68570706+furkandetseli@users.noreply.github.com>
2021-12-05 16:15:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -449,24 +449,24 @@ docReady(function() {
     Logger.logScanStart();
 });
 
-const html5QrCode = new Html5Qrcode(/* element id */ "reader");
-// File based scanning
-const fileinput = document.getElementById('qr-input-file');
-fileinput.addEventListener('change', e => {
-  if (e.target.files.length == 0) {
-    // No file selected, ignore 
-    return;
-  }
+// const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+// // File based scanning
+// const fileinput = document.getElementById('qr-input-file');
+// fileinput.addEventListener('change', e => {
+//   if (e.target.files.length == 0) {
+//     // No file selected, ignore 
+//     return;
+//   }
 
-  const imageFile = e.target.files[0];
-  // Scan QR Code
-  html5QrCode.scanFile(imageFile, true)
-  .then(decodedText => {
-    // success, use decodedText
-    console.log(decodedText);
-  })
-  .catch(err => {
-    // failure, handle it.
-    console.log(`Error scanning file. Reason: ${err}`)
-  });
-});
+//   const imageFile = e.target.files[0];
+//   // Scan QR Code
+//   html5QrCode.scanFile(imageFile, true)
+//   .then(decodedText => {
+//     // success, use decodedText
+//     console.log(decodedText);
+//   })
+//   .catch(err => {
+//     // failure, handle it.
+//     console.log(`Error scanning file. Reason: ${err}`)
+//   });
+// });
